edbec9144092d523feb45735ca634516655bee91
Milan Kubin <milan.kubin@gmail.com>
2014-04-04 03:19:53 +0000

    attempting to fix hstore array search

--- lib/surus/hstore/scope.rb
+++ lib/surus/hstore/scope.rb
@@ -6,7 +6,7 @@ module Surus
        
       def hstore_array_has_any(column, key, value)
        # where("#{connection.quote_column_name(column)} ?& ARRAY[:keys]", :keys => keys.flatten)
-        where("? = ALL (SELECT unnest(\"#{connection.quote_column_name(column)}\") @> ?)", value, key)
+        where("? = ALL (SELECT unnest(#{connection.quote_column_name(column)}) @> ?)", value, key)
       end
       
       
