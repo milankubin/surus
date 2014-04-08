a1da922e89f89b0f57d87a5e3743cf25eb26036d
Milan Kubin <milan.kubin@gmail.com>
2014-04-08 14:13:41 +0000

    fixing hstore array full text search

--- lib/surus/hstore/scope.rb
+++ lib/surus/hstore/scope.rb
@@ -5,8 +5,7 @@ module Surus
        #scope :where_all, ->(column, key, value) { 
        
       def hstore_array_has_any(column, value)
-       # where("#{connection.quote_column_name(column)} ?& ARRAY[:keys]", :keys => keys.flatten)
-        where(" exists ( select * from  (SELECT svals(unnest(#{connection.quote_column_name(column)} ))) x(item) where x.item ILIKE :value ) ", :value => value)
+        where(" exists ( select * from  (SELECT svals(unnest(#{connection.quote_column_name(column)} ))) x(item) where x.item -> :value ) ", :value => value)
       end
       
       
