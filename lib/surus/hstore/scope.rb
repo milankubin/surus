70d73e8ea4e6108871eb228bd2a3e453433af167
Milan Kubin <milan.kubin@gmail.com>
2015-10-21 12:44:53 +0000

    add some not functions for hstore array matchers, tidy up

--- lib/surus/hstore/scope.rb
+++ lib/surus/hstore/scope.rb
@@ -1,29 +1,12 @@
 module Surus
   module Hstore
     module Scope
-      
-       #scope :where_all, ->(column, key, value) { 
-       
-      def hstore_array_has_any(column, value)
-        where(" exists ( select * from  (SELECT svals(unnest(#{connection.quote_column_name(column)} ))) x(item) where x.item ILIKE :value ) ", :value => value)
-      end
-      
-      
-   #   def hstore_has_any_keys(column, *keys)
-    #    where("#{connection.quote_column_name(column)} ?| ARRAY[:keys]", :keys => keys.flatten)
-    #  end
-      
-      
       # Adds a where condition that requires column to contain hash
       #
       # Example:
       #   User.hstore_has_pairs(:properties, "favorite_color" => "green")
       def hstore_has_pairs(column, hash)
         where("#{connection.quote_column_name(column)} @> ?", Serializer.new.dump(hash))
-      end
-      
-      def hstore_array_has_pairs(column, hash)
-        where(" exists (  SELECT * FROM  ( SELECT  hstore(unnest(#{connection.quote_column_name(column)})))  x(item)  WHERE x.item  @> ?)", Serializer.new.dump(hash) );
       end
       
       # Adds a where condition that requires column to contain key
@@ -50,6 +33,22 @@ module Surus
       #    User.hstore_has_any_keys(:properties, ["favorite_color", "favorite_song"])
       def hstore_has_any_keys(column, *keys)
         where("#{connection.quote_column_name(column)} ?| ARRAY[:keys]", :keys => keys.flatten)
+      end
+      
+      def hstore_array_has_pairs(column, hash)
+        where(" exists (  SELECT * FROM  ( SELECT  hstore(unnest(#{connection.quote_column_name(column)})))  x(item)  WHERE x.item  @> ?)", Serializer.new.dump(hash) );
+      end
+      
+      def hstore_array_not_has_pairs(column, hash)
+        where("not exists (  SELECT * FROM  ( SELECT  hstore(unnest(#{connection.quote_column_name(column)})))  x(item)  WHERE x.item  @> ?)", Serializer.new.dump(hash) );
+      end
+    
+      def hstore_array_has_any(column, value)
+        where(" exists ( select * from  (SELECT svals(unnest(#{connection.quote_column_name(column)} ))) x(item) where x.item ILIKE :value ) ", :value => value)
+      end
+      
+      def hstore_array_not_has_any(column, value)
+        where(" not exists ( select * from  (SELECT svals(unnest(#{connection.quote_column_name(column)} ))) x(item) where x.item ILIKE :value ) ", :value => value)
       end
     end
   end
